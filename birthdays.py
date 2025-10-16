451000ba8d77acdff321638c54f6b4586c91e372
KesavThiru <kesav.thiru@hotmail.com>
2025-10-16 15:21:52 +1000

    Add files via upload

--- birthdays.py
+++ birthdays.py
@@ -1,11 +1,12 @@
-# ask for birthday info 5 times
-print ("what is the name of freind 1?")
-print ("what is the birthday of freind 1?")
-print ("what is the name of freind 2?")
-print ("what is the birthday of freind 2?")
-print ("what is the name of freind 3?")
-print ("what is birthday of freind 3")
-print ("what is the name of freind 4")
-print ("what is the birthday of freind 4")
-print ("what is the name of freind 5")
-print ("what is the birthday of freind 5")
+Kesav = ("19 august 2013")
+Saad = ('14 may 2014')
+Max = ("14 may 2014")
+Oliver = ("27 jannuary 2014")
+Taalzhini = ("17 jaunnuary 2021")
+print ("Kesav's birthday is on",Kesav )
+print ("Saad's birthday is on",Saad )
+print ("Max's birthday is on",Max )
+print ("Oliver's birthday is on",Oliver)
+print ("Taalzhini's birthday is on",Taalzhini)
+
+
